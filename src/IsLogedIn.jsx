84aff6bd9bbf7f28a4d6867b9782b573ee0019a6
藤島太智 <fujishimataichi@fujishimataichinoMacBook-puro.local>
2020-11-19 22:16:15 +0900

    2020/11/19

--- src/IsLogedIn.jsx
+++ src/IsLogedIn.jsx
@@ -1,9 +1,20 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import {Redirect} from 'react-router-dom'
 import {AuthContext} from './Auth'
+import firebase from './firebase'
 
 const IsLogedIn = (props) => {
-    const {user}=useContext(AuthContext)
+    const {user, setUser}=useContext(AuthContext)
+    
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+              if(user) {
+                  setUser(true)
+              } else {
+                  setUser(false)
+              }
+          });
+    }, [])
 
     return (
         user? props.children : <Redirect to={'/login'}/>
